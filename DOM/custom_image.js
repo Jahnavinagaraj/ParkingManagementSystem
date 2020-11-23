1753dffcebb1d806c01e9fb3e1e12a514366418a
Jahnavinagaraj <68998183+Jahnavinagaraj@users.noreply.github.com>
2020-11-23 15:07:44 +0530

    updated js file

--- DOM/custom_image.js
+++ DOM/custom_image.js
@@ -14,12 +14,12 @@ function loadImage(xml)
 	xmlDoc=xml.responseXML;
 	
 	var banner=xmlDoc.getElementsByTagName("banner");
-	document.getElementById("banner").setAttribute("src",banner[0].childnodes[0].nodeValue);
+	document.getElementById("banner").setAttribute("src",banner[0].childNodes[0].nodeValue);
 	
 	var sideImage = xmlDoc.getElementsByTagName("parkingimage");
 	document.getElementById("secondimage").style.background = "url("+secondimage[0].childNodes[0].value+")";
 	
 	var introVideo = xmlDoc.getElementsByTagName("advideo");
-	document.getElementById("advideo").setAttribute("src",introVideo[0].childnodes[0].nodeValue);
+	document.getElementById("advideo").setAttribute("src",introVideo[0].childNodes[0].nodeValue);
 }
 
