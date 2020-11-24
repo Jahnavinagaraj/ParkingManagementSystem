9ea8238a61603507d83f6979b2bb17a2287b6d22
jahnavinagaraj <jahnavi.n@mca.christuniversity.com>
2020-11-24 11:28:46 +0530

    Modified form validations for member creation

--- assignment6-cookies/js/formvalidator.js
+++ assignment6-cookies/js/formvalidator.js
@@ -2,11 +2,18 @@ function validateName(){
 	var name_js = document.getElementById("name");
     name_js.value = name_js.value.toUpperCase();
     var nameLength = name_js.value.length
-    if (nameLength < 3){
+    if(!/^[a-zA-Z ]+$/.test(name_js.value)){
+		document.getElementById('nameerror').innerHTML= "Name should contain only alphabets and spaces";
+	    document.getElementById("name").style.borderColor = "red";
+		return false;
+    }
+    else if (nameLength < 3 ){
+        document.getElementById('nameerror').innerHTML= "Name should be at least 3 characters length";
 	    document.getElementById("name").style.borderColor = "red";
 		return false;
     }
     else{
+		document.getElementById('nameerror').innerHTML="";
         document.getElementById("name").style.borderColor = "green";
 		return true;
     }
@@ -16,26 +23,40 @@ function validateVehicleNo(){
 	var vehicalNumber = document.getElementById("vehical_number");
     vehicalNumber.value = vehicalNumber.value.toUpperCase();
     var vehicleLength = vehicalNumber.value.length
-    if (vehicleLength < 3){
+    
+    if(!/^[a-zA-Z0-9 ]+$/.test(vehicalNumber.value)){
+		document.getElementById('veherror').innerHTML= "Vehicle number should be alphanumeric only";
+	    document.getElementById("vehical_number").style.borderColor = "red";
+		return false;
+    }
+    else if (vehicleLength < 3){
+		document.getElementById('veherror').innerHTML= "Vehicle Number should be at least 3 characters length";
 	    document.getElementById("vehical_number").style.borderColor = "red";
 		return false;
     }
     else{
+		document.getElementById('veherror').innerHTML="";
         document.getElementById("vehical_number").style.borderColor = "green";
 		return true;
     }
 }
 
 function setColorPhone(){
-   var phn = document.getElementById("phone").value;
-   var phnLength = phn.length
-   if (phnLength!=10){
-	   document.getElementById("phone").style.borderColor = "red";
+	var phn = document.getElementById("phone").value;
+	var phnLength = phn.length
+	if (isNaN(phn)){
+		document.getElementById('phnerror').innerHTML= "Phone number should be numeric only.";
+		document.getElementById("phone").style.borderColor = "red";
+	}
+	else if (phnLength!=10){
+		document.getElementById('phnerror').innerHTML= "Phone number should be 10 numbers.";
+		document.getElementById("phone").style.borderColor = "red";
 	   return false;
-   }
-   else{
-   document.getElementById("phone").style.borderColor = "green";
-   return true;
+	}
+	else{
+		document.getElementById('phnerror').innerHTML= "";
+		document.getElementById("phone").style.borderColor = "green";
+		return true;
    }
 }
 
@@ -43,12 +64,14 @@ function validateEmail(){
    var emailField = document.getElementById("email");
    isValidEmail= emailField.checkValidity();
    if (!isValidEmail){
-	   document.getElementById("email").style.borderColor = "red";
+		document.getElementById('emailerror').innerHTML= "Not a valid email id.";
+		document.getElementById("email").style.borderColor = "red";
 	   return false;
    }
    else{
-       document.getElementById("email").style.borderColor = "green";
-	   return true;
+		document.getElementById('emailerror').innerHTML= "";
+		document.getElementById("email").style.borderColor = "green";
+		return true;
    }
 }
 
@@ -57,12 +80,14 @@ function validateAddress(){
 
     var textArea = address.value.length;
     var charactersLeft = 200 - textArea;
-    var count = document.getElementById('remainingC');
-    count.innerHTML = "Characters left: " + charactersLeft;
-	if (address.value.length=0){
-	  return false;
+	if (address.value.length<20){
+		document.getElementById('remainingC').innerHTML="";
+		document.getElementById('addresserror').innerHTML= " Address should be at least 20 characters length";
+		return false;
 	}
 	else {
+		document.getElementById('remainingC').innerHTML = "Characters left: " + charactersLeft;
+		document.getElementById('addresserror').innerHTML= "";
 		return true;
 	}
 }
