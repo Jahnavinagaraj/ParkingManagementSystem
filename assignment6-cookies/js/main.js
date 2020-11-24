c8830e2c83b4fd0d63da19611ecc26fe5675d6de
jahnavinagaraj <jahnavi.n@mca.christuniversity.com>
2020-11-24 08:58:04 +0530

    Added login and inactive pages with cookies to check session

---
+++ assignment6-cookies/js/main.js
@@ -0,0 +1,45 @@
+function checkLogin(form)
+{
+	if(form.userid.value == "admin" && form.pwd.value == "password")
+	{
+		isSessionCreated = createSession();
+		
+		if (isSessionCreated){
+			window.location.href = "home.html";
+			return false;
+		}
+		else{
+			alert("Cookie NOT set. Please make sure your browser is accepting cookies.");
+			return false;
+		}	
+		
+	}
+	else
+	{
+		alert("Either Password or Username is wrong");
+		return false;
+	}
+}
+
+function checkSession(){
+	if (document.cookie && document.cookie.indexOf('SessionAlive=1') != -1) {
+	    createSession();
+		return true;
+	}
+	else {
+	 window.location.href = "inactive.html";
+	}
+
+}
+
+function createSession(){
+	var secs = 30;
+	var now = new Date();
+    var exp = new Date(now.getTime() + secs*1000);
+    document.cookie = 'SessionAlive=1; expires='+exp.toUTCString();
+	if (document.cookie && document.cookie.indexOf('SessionAlive=1') != -1) {
+        return true;
+    } else {
+        return false;
+    }
+}
